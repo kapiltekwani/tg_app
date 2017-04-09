88dcd2f354930c131006e84d825e7553d78b2da2
Ubuntu <ubuntu@theonemirror.eng.vmware.com>
2017-04-09 03:27:49 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TradeGeckoApp::Application.config.secret_token = 'f19c904b7feda6bc5fd43cea98492cff52c599bceaf7fdaaafb2b897be89db1ad45e9b3694441d9102064861ee64989125c7d68686d9a08afc47646664874c4c'
